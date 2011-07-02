67c3c3734855cb54117a620e898fb79e2018507c
Kenta Sato <karupa@cpan.org>
2011-07-02 22:35:32 +0900

    initial commit

---
+++ lib/Atelier.pm
@@ -0,0 +1,32 @@
+package Atelier;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Atelier -
+
+=head1 SYNOPSIS
+
+  use Atelier;
+
+=head1 DESCRIPTION
+
+Atelier is
+
+=head1 AUTHOR
+
+Kenta Sato E<lt>karupa@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
